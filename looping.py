44b6a09d93a6b278e1b41cc8a41d3fb5d2b89e3a
aryanharitsh123 <aryanharitsh@gmail.com>
2018-04-08 12:58:33 +0530

    looping through a list

---
+++ looping.py
@@ -0,0 +1,10 @@
+list = [1,2,3]
+for i in list:
+        if i == 2:
+            print("yep, its two")
+        elif  i == 1:
+            print("yes its 1")
+        else:
+           print(i)
+
+       
