278b424940ec146c6dbe7f48c5d1b750971c18bf
aryanharitsh123 <aryanharitsh@gmail.com>
2018-04-09 12:10:32 +0530

    divides a list into str and int

---
+++ parsingfunction.py
@@ -0,0 +1,17 @@
+items=["Aman","Aryan","Justin",656,963.5,"Another"]
+list_str=[]
+list_int=[]
+
+def parse_list(abc):
+	for x in items: 
+		if isinstance(x, int) or isinstance(x, float):
+			list_int.append(x)
+		elif isinstance(x, str):
+			list_str.append(x)
+	else:
+			pass
+	return list_str, list_int		
+
+
+print(parse_list(items))
+
