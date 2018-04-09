0896073af95d29891e60db0c9a4f8d9f498a9a10
aryanharitsh123 <aryanharitsh@gmail.com>
2018-04-09 09:23:21 +0530

    Own try

---
+++ finally.py
@@ -0,0 +1,13 @@
+match=0
+list_a=["Aryan","Aman",85,"Another",96]
+list_b=[]
+for x in range(len(list_a)):
+	x=x-match
+	print(list_a[x])
+	print(x)
+	if isinstance(list_a[x], int):
+	   list_b.append(list_a[x])   
+	   list_a.pop(x)
+	   match = match + 1
+print(list_a)
+print(list_b)
